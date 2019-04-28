528112a22d9bd493a595eb6a9a58aa7d417d5ca6
johannesridho <johanridho@gmail.com>
2019-04-28 01:58:03 -0700

    Add saveLabelDetection stitch

--- stitch/saveLabelDetection.js
+++ stitch/saveLabelDetection.js
@@ -2,12 +2,12 @@ exports = function(payload) {
     const jsonBody = EJSON.parse(payload.body.text())
 
     const mongodb = context.services.get("citiv")
-    const collection = mongodb.db("citiv").collection("recognition")
+    const collection = mongodb.db("citiv").collection("crime-detection")
 
-    for (idx in jsonBody.criminals) {
-        newRecognition = {
+    for (idx in jsonBody.labels) {
+        newItem = {
             "owner_id": 1,
-            "criminal": jsonBody.criminals[idx],
+            "label": jsonBody.labels[idx],
             "job_id": jsonBody.job_id,
             "cctv_id": jsonBody.cctv_id,
             "latitude": jsonBody.latitude,
@@ -17,7 +17,7 @@ exports = function(payload) {
             "city": jsonBody.city
         }
 
-        collection.insertOne(newRecognition)
+        collection.insertOne(newItem)
             .then(result => console.log(`Successfully inserted item with _id: ${result.insertedId}`))
             .catch(err => console.error(`Failed to insert item: ${err}`))
     }
